81ea217f5bc0885e7ab68ee49ddf1c8540754665
Isa Abutaa <isaabutaa@gmail.com>
2019-11-13 16:14:34 -0700

    grocery object and daily editor

---
+++ exercises/grocery-list/app.js
@@ -0,0 +1,11 @@
+var shopper = {
+    name:'Nanny McFee',
+    age: 99,
+    isMagical: true,
+    specialPower: function () {
+        console.log('I am Nanny McFee, and I can fly through the clouds!')
+    },
+    groceryCartItems: ['bananas','greek yoghurt','frozen berries','low-fat milk']
+}
+
+shopper.specialPower()
