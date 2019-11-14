26cb0af9481b4fc46e96fd99857409f2e739433a
Isa Abutaa <isaabutaa@gmail.com>
2019-11-14 14:08:48 -0700

    if-statement-olympics using ternary syntax

---
+++ exercises/if-statement-olympics/app.js
@@ -0,0 +1,41 @@
+//preliminaries
+
+// 5 > 3 ? console.log('is greater than') : console.log('is not greater than')
+
+// 'cat'.length === 3 ? console.log('is the length') : console.log('is not the length')
+
+// 'cat' === 'dog' ? console.log('is the same') : console.log('is not the same')
+
+// bronze medal
+
+// var person = {
+//     name: "Bobby",
+//     age: 12
+// }
+
+// person.age >= 18 ? console.log(person.name + ' is old enough') : console.log(person.name + ' is not old enough')
+
+// person.name[0] === 'B' ? console.log(person.name + ' can see the movie') : console.log(person.name + ' CANNOT see the movie.')
+
+// person.name[0] === 'B' && person.age >= 18 ? console.log(person.name + ' can go.') : console.log(person.name + ' cannot go.')
+
+//silver medal
+
+// 1 === '1' ? console.log('strict') : 1 == '1' ? console.log('loose') : console.log('not equal at all')
+
+// 1 <= 2 && 2 === 4 ? console.log('yes') : console.log('hell no')
+
+//gold medal
+
+typeof 'dog' === 'string' ? console.log('is a string') : console.log("isn't a string")
+
+typeof true === 'boolean' ? console.log('is a boolean') : console.log('is not a boolean')
+
+var myDigit = 24
+24 % 2 === 0 ? console.log('even number') : console.log('odd number')
+
+var car
+car == undefined ? console.log('is undefined') : console.log('is defined')
+
+'s' > 12 ? console.log('is greater than') : console.log('not the same data type')
+
