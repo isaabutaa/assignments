f2bde1ae671b8d52ad474d4488edc6455c489bfc
Isa Abutaa <isaabutaa@gmail.com>
2019-11-21 12:07:19 -0700

    better-mario-pest-control, needs more styling

---
+++ projects/better-mario-pest-control/app.js
@@ -0,0 +1,20 @@
+var baddieForm = document["baddie-form"]
+
+baddieForm.addEventListener("submit", function(event){
+    event.preventDefault()
+    var coinsForGoombas = Number(baddieForm["goombas-squashed"].value) * 5
+    var coinsForBobBombs = Number(baddieForm["bob-bombs-squashed"].value) * 7
+    var coinsForCheepCheeps = Number(baddieForm["cheep-cheeps-squashed"].value) * 11
+    var totalCoins = coinsForGoombas + coinsForBobBombs + coinsForCheepCheeps
+    // 1. create element that will display the result of coins owed to Mario
+    var coinsH3 = document.createElement("h3")
+
+    // 2. edit the <h3></h3> element
+    coinsH3.textContent = "Peach owes Mario a total of " + totalCoins + " coins!"
+    coinsH3.className = "total-coins-h3"
+
+    // 3. Append coinsH3 to the DOM -- append it to the div with a class of "coins-list"
+    var coinsList = document.getElementById("coins-list")
+    coinsList.appendChild(coinsH3)
+    console.log(coinsH3)
+})
--- projects/mario-pest-control/app.js
+++ projects/mario-pest-control/app.js
@@ -1,5 +1,5 @@
-var baddieContainer = document.getElementById("baddie-rate-container")
-var squashedContainer = document.getElementById("squashedContainer")
+// var baddieContainer = document.getElementById("baddie-rate-container")
+// var squashedContainer = document.getElementById("squashedContainer")
 // pull the elements from the DOM that contain the number inputs
 // these will be used to calculate the total number of coins that Peach owes Mario
 
@@ -8,27 +8,46 @@ var submitBtn = document.getElementById("submit-num-squashed")
 submitBtn.addEventListener("click", function(e){
     e.preventDefault()
     // third step: for each of the forms, create variables, then create variables inside the forms that will save the input values
+    var squashedForm = document.squashed
     //goombas for and input
-    var goombas = document.goombasForm
-    var goombasSquashed = Number(goombas.value) * 5
-    goombas.appendChild(goombasSquashed)
+    var goombasSquashed = Number(squashedForm.goombasSquashed.value) * 5
     //bobombs form and input
-    var bobombs = document.bobombsForm
-    var bobombsSquashed = Number(bobombs.value) * 7
-    bobombs.appendChild(bobombsSquashed)
+    var bobombsSquashed = Number(squashedForm.bobombsSquashed.value) * 7
     //cheep-cheeps form and input
-    var cheeps = document.cheepsForm
-    var cheepsSquashed = Number(cheeps.value) * 11
-    cheeps.appendChild(cheepsSquashed)
+    var cheepsSquashed = Number(squashedForm.cheepsSquashed.value) * 11
     // fourth step: create a variable that will contain the final value of the inputs
-    var totalSquashed = goombas + bobombs + cheeps
+    var totalSquashed = goombasSquashed + bobombsSquashed + cheepsSquashed
 
     //fifth step: create an element that will contain the total in text, add textContent
     var totalCoins = document.createElement("div")
     totalCoins.textContent = "Peach owes Mario: " + totalSquashed + " coins!"
-    squashedContainer.appendChild(totalCoins)
+    squashed.appendChild(totalCoins)
 })
 
 // second step: getElementsByClassName
 // var squashedArr = document.getElementsByClassName("squashed")
 
+
+// Try this: Instead of using three forms for the inputs, just create one form with three inputs. Create separate variables for the input values, add them together, and append them to the form.
+
+
+// // third step: for each of the forms, create variables, then create variables inside the forms that will save the input values
+//     //goombas for and input
+//     var goombas = document.goombasForm
+//     var goombasSquashed = Number(goombas.value) * 5
+//     goombas.appendChild(goombasSquashed)
+//     //bobombs form and input
+//     var bobombs = document.bobombsForm
+//     var bobombsSquashed = Number(bobombs.value) * 7
+//     bobombs.appendChild(bobombsSquashed)
+//     //cheep-cheeps form and input
+//     var cheeps = document.cheepsForm
+//     var cheepsSquashed = Number(cheeps.value) * 11
+//     cheeps.appendChild(cheepsSquashed)
+//     // fourth step: create a variable that will contain the final value of the inputs
+//     var totalSquashed = goombas + bobombs + cheeps
+
+//     //fifth step: create an element that will contain the total in text, add textContent
+//     var totalCoins = document.createElement("div")
+//     totalCoins.textContent = "Peach owes Mario: " + totalSquashed + " coins!"
+//     squashedContainer.appendChild(totalCoins)
