d9252ffef823792072967c9fe4a8fd13a1d03646
Isa Abutaa <isaabutaa@gmail.com>
2019-11-22 10:31:12 -0700

    readline-sync calculator

---
+++ exercises/node-js-calculator/app.js
@@ -0,0 +1,33 @@
+var readline = require("readline-sync")
+
+// console.dir(readline)
+
+console.log("Hello, user. I am the supreme program of the internet. I will ask you a few questions, and you will answer.")
+
+var userName = readline.question("Tell me, what is your name? ")
+console.log("It is wonderful to meet you " + userName + ".")
+
+var firstNumber = Number(readline.question("Please enter a number value. "))
+
+var secondNumber = Number(readline.question("Wonderful, your first number is " + firstNumber + ". Now enter a second number. "))
+
+var mathOperations = ["add", "subtract", "multiply", "divide"]
+
+var mathChoice = readline.keyInSelect(mathOperations, "Now please choose the math operation that you would like to perform with " + firstNumber + " and " + secondNumber + ".")
+
+var mathAnswer = function() {
+    if(mathOperations[mathChoice] === "add"){
+        return firstNumber + secondNumber
+    } else if(mathOperations[mathChoice] === "subtract") {
+        return firstNumber - secondNumber
+    } else if(mathOperations[mathChoice] === "multiply") {
+        return firstNumber * secondNumber
+    } else {
+        return firstNumber / secondNumber
+    }
+}
+
+console.log("The answer to your math operation is " + mathAnswer())
+
+
+
---
+++ exercises/travel-form/app.js
@@ -0,0 +1,13 @@
+var travelForm = document["travel-form"]
+console.log(travelForm)
+
+//create addEventListener
+travelForm.addEventListener("submit",function(e){
+    var firstName = travelForm["first-name"].value
+    var lastName = travelForm["last-name"].value
+    var age = travelForm["age"].value
+    var male = travelForm["male"]
+    var female = travelForm["female"]
+    var other = travelForm["other"]
+    
+})
