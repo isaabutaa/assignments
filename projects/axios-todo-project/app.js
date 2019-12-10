ea203636a5ca7879d8addef103f44e2d3bed880a
Isa Abutaa <isaabutaa@gmail.com>
2019-12-10 12:10:38 -0700

    I finished my to-do list project, and I am proud! I can add new to-do items, I can update them, I can delete them, and I can move them back and forth between a completed and not-completed section.

--- projects/axios-todo-project/app.js
+++ projects/axios-todo-project/app.js
@@ -1,6 +1,8 @@
 // url: https://api.vschool.io/isa-man/todo
 
 // Pt. 1 - GET
+getToDos()
+
 function getToDos() {
     axios.get("https://api.vschool.io/isa-man/todo")
         .then(response => listToDos(response.data))
@@ -9,7 +11,7 @@ function getToDos() {
 
 function listToDos(arr) {
     for(let i = 0; i < arr.length; i++){
-        const listedItem = document.createElement("form")
+        const listedItem = document.createElement("div")
         const checkbox = document.createElement("input")
         checkbox.type = "checkbox"
         const toDoItem = document.createElement("h3")
@@ -18,45 +20,92 @@ function listToDos(arr) {
         description.textContent = arr[i].description
         const updateBtn = document.createElement("button")
         updateBtn.textContent = "update"
+        const deleteBtn = document.createElement("button")
+        deleteBtn.textContent = "delete"
         listedItem.appendChild(checkbox)
         listedItem.appendChild(toDoItem)
         listedItem.appendChild(description)
         listedItem.appendChild(updateBtn)
-        document.getElementById("to-do-container").appendChild(listedItem)
+        listedItem.appendChild(deleteBtn)
+        isCompleted()
 
-        checkbox.addEventListener("click", () => {
-            document.getElementById("completed-container").appendChild(listedItem)
+        checkbox.addEventListener("click", (e) => {
+            completedUpdate()
+        })
 
-            // axios.put("https://api.vschool.io/isa-man/todo")
-            //     .then(response => completeToDos(response.data))
-            //     .catch(error => console.log(error))
+        deleteBtn.addEventListener("click", () => {
+            axios.delete(`https://api.vschool.io/isa-man/todo/${arr[i]._id}`)
+                .then(response => console.log(response))
+                .catch(error => console.log(error))
+            listedItem.remove()
+        })
+
+        updateBtn.addEventListener("click", () => {
+            listedItem.removeChild(updateBtn)
+            listedItem.removeChild(deleteBtn)
+            const updateInput = document.createElement("input")
+            updateInput.type = "text"
+            updateInput.placeholder = "change to-do item name"
+            const newUpdateBtn = document.createElement("button")
+            newUpdateBtn.textContent = "update to-do item"
+            listedItem.appendChild(updateInput)
+            listedItem.appendChild(newUpdateBtn)
+            newUpdateBtn.addEventListener("click", (e) => {
+                e.preventDefault()
+                const titleUpdate = {
+                    title: updateInput.value
+                }
+
+                axios.put(`https://api.vschool.io/isa-man/todo/${arr[i]._id}`, titleUpdate)
+                    .then(response => {
+                        listToDos([response.data])
+                    })
+                    .catch(error => console.log(error))
+
+                listedItem.remove()
             })
-            
+        })
+        
+        function completedUpdate() {
+            const completeUpdate = {
+                completed: false
+            }
+
+            if(arr[i].completed){
+                completeUpdate.completed = false
+                document.getElementById("to-do-container").appendChild(listedItem)
+            } else {
+                completeUpdate.completed = true
+                document.getElementById("completed-container").appendChild(listedItem)
+            }
+
+            axios.put(`https://api.vschool.io/isa-man/todo/${arr[i]._id}`, completeUpdate)
+                .then(response => {
+                    listToDos([response.data])
+                    // console.log(response)
+                })
+                .catch(error => console.log(error))
+
+            listedItem.remove()
+        }
+
+        function isCompleted() {
+            if(arr[i].completed){
+                checkbox.checked = true
+                document.getElementById("completed-container").appendChild(listedItem)
+
+            } else {
+                document.getElementById("to-do-container").appendChild(listedItem)
+            }
+        }     
     }
 }
-
-getToDos()
-
 
 // Pt. 2 - POST
 
 const addForm = document.addForm
 addForm.addEventListener("submit", function (event) {
     event.preventDefault()
-    const listedItem = document.createElement("form")
-    const checkbox = document.createElement("input")
-    checkbox.type = "checkbox"
-    const toDoItem = document.createElement("h3")
-    toDoItem.textContent = addForm.newItem.value
-    const description = document.createElement("p")
-    description.textContent = addForm.description.value
-    const updateBtn = document.createElement("button")
-    updateBtn.textContent = "update"
-    listedItem.appendChild(checkbox)
-    listedItem.appendChild(toDoItem)
-    listedItem.appendChild(description)
-    listedItem.appendChild(updateBtn)
-    document.getElementById("to-do-container").appendChild(listedItem)
 
     const newToDo = {
         title: addForm.newItem.value,
@@ -66,46 +115,8 @@ addForm.addEventListener("submit", function (event) {
     addForm.description.value = ""
 
     axios.post("https://api.vschool.io/isa-man/todo",newToDo)
-        .then(response => console.log(response))
+        .then(response => {
+            listToDos([response.data])
+        })
         .catch(error => console.log(error))
-
 })
-
-// Pt. 3 - PUT
-
-
-
-
-
-
-
-
-// const addForm = document.addForm
-// const plusBtn = document.getElementById("plus-button")
-// plusBtn.addEventListener("click", (event) => {
-//     event.preventDefault()
-//     const newToDoInput = document.createElement("input")
-//         newToDoInput.type = "text"
-//         newToDoInput.placeholder = "new to-do item"
-//     const addItemBtn = document.createElement("button")
-//         addItemBtn.textContent = "Add to list"
-//     document.addForm.appendChild(newToDoInput)
-//     document.addForm.appendChild(addItemBtn)
-    
-
-//     addItemBtn.addEventListener("click", (event) => {
-//         event.preventDefault()
-//         const listedItem = document.createElement("form")
-//         const newToDoItem = document.createElement("h3")
-//             newToDoItem.textContent = newToDoInput.value
-//             newToDoInput.value = ""
-//         const updateBtn = document.createElement("button")
-//             updateBtn.textContent = "update"
-//         listedItem.appendChild(newToDoItem)
-//         listedItem.appendChild(updateBtn)
-//         document.getElementById("to-do-container").appendChild(listedItem)
-
-//     })
-
-// })
-
