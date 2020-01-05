31c50db6f3903d060306c5633da2af7631ccac5e
Isa Abutaa <isaabutaa@gmail.com>
2020-01-05 15:31:20 -0700

    tried adding edit function

--- exercises/ugly-things/src/CuteThing.js
+++ exercises/ugly-things/src/CuteThing.js
@@ -4,18 +4,18 @@ import {CuteThingConsumer} from "./CuteThingContext"
 
 function CuteThing(props) {
     return (
-            <CuteThingConsumer>
-                 {({removeCuteThing}) => (
-                    <div className="cute-thing">
-                        <h3>{props.cuteThing.title}</h3>
-                        <p> {props.cuteThing.description} </p>
-                        <img src={props.cuteThing.imgUrl} alt="" />
-                        <br />
-                        <button>Edit</button>
-                        <button onClick={() => {removeCuteThing(props.cuteThing.title)}}>Delete</button>
-                    </div>
-                 )}
-            </CuteThingConsumer>
+        <CuteThingConsumer>
+                {({removeCuteThing, editCuteThing}) => (
+                <div className="cute-thing">
+                    <h3>{props.cuteThing.title}</h3>
+                    <p> {props.cuteThing.description} </p>
+                    <img src={props.cuteThing.imgUrl} alt="" />
+                    <br />
+                    <button onClick={() => {editCuteThing(props.cuteThing.title)}}>Edit</button>
+                    <button onClick={() => {removeCuteThing(props.cuteThing.title)}}>Delete</button>
+                </div>
+                )}
+        </CuteThingConsumer>
     )
 }
 
--- exercises/ugly-things/src/CuteThingContext.js
+++ exercises/ugly-things/src/CuteThingContext.js
@@ -4,6 +4,9 @@ const {Provider, Consumer} = React.createContext()
 
 class CuteThingProvider extends Component {
     state = {
+        title: "",
+        description: "",
+        imgUrl: "",
         cuteThings: []
     }
 
@@ -15,9 +18,30 @@ class CuteThingProvider extends Component {
         this.setState(prevState => ({cuteThings: prevState.cuteThings.filter(item => item.title !== title)}))
     }
 
+    editCuteThing = (title) => {
+        this.setState(prevState => ({cuteThings: prevState.cuteThings.map(item => {
+            if(item.title === title) {
+                return (
+                    <div>
+                        <input type="text" name="title" value={this.state.title} placeholder="new title" onChange={this.handleChange} />
+                        <input type="text" name="description" value={this.state.description} placeholder="new description" onChange={this.handleChange} />
+                        <input type="text" name="imgUrl" value={this.state.imgUrl} placeholder="new imgUrl" onChange={this.handleChange} />
+                        <button>Edit item</button>
+                    </div>
+                )
+            }
+            return item
+        })}))
+    }
+
     render() {
         return (
-            <Provider value={{cuteThings: this.state.cuteThings, addCuteThing: this.addCuteThing, removeCuteThing: this.removeCuteThing}}>
+            <Provider value={{
+                        cuteThings: this.state.cuteThings, 
+                        addCuteThing: this.addCuteThing, 
+                        removeCuteThing: this.removeCuteThing,
+                        editCuteThing: this.editCuteThing
+                        }}>
                 {this.props.children}
             </Provider>
         )
